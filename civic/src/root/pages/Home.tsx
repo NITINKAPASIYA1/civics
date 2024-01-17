241c27f4cddeb4cc20a43a44aa8967e13eabe726
NITIN KAPASIYA <kumarnitin5814@gmail.com>
2024-01-17 17:13:39 +0530

    Update Home.tsx

--- civic/src/root/pages/Home.tsx
+++ civic/src/root/pages/Home.tsx
@@ -2,17 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="flex flex-1">
-      <div className="home-container">
-        <div className="home-posts">
-          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
-        </div>
-      </div>
-
-      <div className="home-creators">
-        <h3 className="h3-bold text-light-1">Top Creators</h3>
-      </div>
-    </div>
+    <div>Home</div>
   )
 }
 
